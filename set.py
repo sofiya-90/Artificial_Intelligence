50361a1a1cea240de210f15b1c4b3bad8d9588d5
sofiya-90 <147678746+sofiya-90@users.noreply.github.com>
2024-06-26 17:41:29 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,13 @@
+set_A = {1, 2, 3, 4}
+set_B = {3, 4, 5, 6}
+union_result = set_A | set_B
+print("Union:", union_result)
+
+intersection_result = set_A & set_B
+print("Intersection:", intersection_result)
+
+difference_result = set_A - set_B
+print("Difference (A - B):", difference_result)
+
+symmetric_difference_result = set_A ^ set_B
+print("Symmetric Difference:", symmetric_difference_result)
